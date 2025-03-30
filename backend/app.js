77b26c1b86ef320e8ec51a685fa06e240db5b957
blekmus <dinil@protonmail.com>
2025-03-30 12:27:54 -0400

    allow cors in backend

--- backend/app.js
+++ backend/app.js
@@ -4,21 +4,21 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
 
-const users = [];
+const io = new Server(server, {
+  cors: {
+    origin: "*", // Allow all origins
+    methods: ["GET", "POST"],
+  },
+}); 
 
 io.on("connection", (socket) => {
-  // send messages to all users except the sender
   socket.on("general", ({ username, message }) => {
-    users.push({ id: socket.id, username });
-    socket.broadcast.emit("general", `${username}: ${message}`);
+    socket.broadcast.emit("general", {username, message});
   });
 
   socket.on("lesson", ({ username, message }) => {
-    users.push({ id: socket.id, username });
-    socket.broadcast.emit("lessonusers", username);
-    socket.broadcast.emit("lesson", `${username}: ${message}`);
+    socket.broadcast.emit("lesson", {username, message});
   });
 });
 
@@ -26,6 +26,6 @@ app.get("/", (req, res) => {
   res.send("Socket.IO server is running");
 });
 
-server.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+server.listen(3001, () => {
+  console.log("Server running on http://localhost:3001");
 });
